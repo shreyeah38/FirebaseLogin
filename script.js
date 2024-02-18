2e936b22014e70773a4d6249477a3b8600c8551a
shreya github <shreyarai0308@gmail.com>
2024-02-18 21:27:13 +0530

    login

---
+++ script.js
@@ -0,0 +1,23 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import { getDatabase,ref,set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
+// const firebaseConfig = {
+// apiKey: "AIzaSyB7U7Wh1AAPwHs1pjxCHtv6NGchgPqtI1A",
+// authDomain: "simplelogin-b72fd.firebaseapp.com",
+// projectId: "simplelogin-b72fd",
+// storageBucket: "simplelogin-b72fd.appspot.com",
+// messagingSenderId: "57391818016",
+// appId: "1:57391818016:web:ad69163525c96e6b77a345"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getDatabase(app);
+// document.getElementById("submit").addEventListener('click',function(e){
+//     set(ref(db, 'user/' + document.getElementById("username").value),
+//     {
+//         Username: document.getElementById("username").value,
+//         Email: document.getElementById("email").value,
+//         PhoneNumber: document.getElementById("phone").value,
+//     })
+//     alert("Login Successful")
+// })
